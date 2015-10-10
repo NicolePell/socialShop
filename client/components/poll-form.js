87fe904f5dafa75025d22347f2d11015f19e1acf
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-10-10 15:45:21 +0100

    Updated polls to use automatic saving of choices and removed ability to create optional poll.

---
+++ client/components/poll-form.js
@@ -0,0 +1,19 @@
+Template.pollForm.events({
+
+  'submit form': function(event) {
+
+    event.preventDefault();
+
+    var newPoll = {
+      choices: [
+        {  text: "Hells yeah.", votes: 0 },
+        {  text: "Fugly.", votes: 0 },
+        {  text: "MEH.", votes: 0 }
+      ]
+    };    
+     
+    // create the new poll
+    Polls.insert(newPoll);
+  }
+
+});
