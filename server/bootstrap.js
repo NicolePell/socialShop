18cdcf76f659f91dc5bd77cae13372ce22b7d433
NicolePell <pellicena@gmail.com>
2015-10-11 21:13:28 +0100

    votes working

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -49,5 +49,18 @@ Meteor.methods({
 
     // create the new poll
     Polls.insert(newPoll);
+  },
+
+  incrementVotes: function(pollID, voteID) {
+    // create the incrementing object so we can add to the corresponding vote
+    var voteString = 'choices.' + voteID + '.votes';
+    var action = {};
+    action[voteString] = 1;
+
+    // increment the number of votes for this choice
+    Polls.update(
+      { _id: pollID },
+      { $inc: action }
+    );
   }
 });
